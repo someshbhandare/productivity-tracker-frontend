e7361203bb339ec05c7ba7c637d3d82a6b91cafd
Somesh Bhandare <someshbhandare3@gmail.com>
2022-08-06 17:14:22 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const addActivity = async (event) => {
+  event.preventDefault();
+  const activity = {
+    name: event.target.activity.value,
+    time: event.target.time.value
+  }
+
+  await fetch(`${process.env.REACT_APP_BACKEND_URL}/activity`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(activity)
+  })
+
+  event.target.activity.value = "";
+  event.target.time.value = "";
+  window.location.reload();
+}
 
 function App() {
+  const [activities, setActivities] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`${process.env.REACT_APP_BACKEND_URL}/activities`);
+      const data = await result.json();
+      setActivities(data);
+    }
+    fetchData();
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <h1>Productivity Tracker</h1>
+        <form onSubmit={addActivity}>
+          <div>
+            <label htmlFor="activity">Activity: </label>
+            <input type="text" name="activity" id="activity" />
+          </div>
+          <div>
+            <label htmlFor="time">Time Taken: </label>
+            <input type="text" name="time" id="time" />
+          </div>
+          <button type="submit">Add</button>
+        </form>
       </header>
+      <main className="app-main">
+        <h2>Today: </h2>
+        {activities && activities.length > 0 ? (
+          <ol>
+            {activities.map(activity => (
+              <li key={activity._id} >{activity.name} - {activity.time}</li>
+            ))}
+          </ol>
+        ): (
+          <p>No activities to show</p>
+        )}
+      </main>
     </div>
   );
 }
